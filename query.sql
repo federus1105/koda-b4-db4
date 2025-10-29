cf4f805e7ed2a34dd53f35e9d5aff272dde09531
federus1105 <federusrudi@gmail.com>
2025-10-29 16:20:59 +0700

    Fix ( query ) : column on table users and account

--- query.sql
+++ query.sql
@@ -1,15 +1,16 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
     email VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    id_account INT NOT NULL,
-    Foreign Key (id_account) REFERENCES account(id)
+    password VARCHAR(100) NOT NULL,
+    role VARCHAR(5) DEFAULT 'user'
 );
 
 CREATE TABLE account (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(40) DEFAULT 'user',
+    username VARCHAR(40) DEFAULT 'username',
     phone VARCHAR(12) DEFAULT '081122334455',
+    id_user INT UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Foreign Key (id_user) REFERENCES users(id)
 )
