6e0633b1316d87bfccef76788df79d85bf56d76a
federus1105 <federusrudi@gmail.com>
2025-10-29 15:18:26 +0700

    Feat ( query ) : sintaks query for authentication flow

---
+++ query.sql
@@ -0,0 +1,15 @@
+CREATE TABLE user (
+    id SERIAL PRIMARY KEY, 
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    id_account INT NOT NULL
+    Foreign Key (id_account) REFERENCES account(id)
+);
+
+CREATE TABLE account {
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(40) DEFAULT 'user',
+    phone VARCHAR(12) DEFAULT '081122334455',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+}
